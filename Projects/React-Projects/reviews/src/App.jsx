a512cacab26ef50aa02186c0a7b36dfa35a691fd
joydeepdev <joydeep6123@gmail.com>
2026-05-26 23:53:23 +0530

    feat(React-Projects/reviews): next and prev user button done

--- Projects/React-Projects/reviews/src/App.jsx
+++ Projects/React-Projects/reviews/src/App.jsx
@@ -5,6 +5,28 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) return 0;
+    if (number < 0) return people.length - 1;
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((c) => {
+      const newIndex = c + 1;
+
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((c) => {
+      const newIndex = c - 1;
+
+      return checkNumber(newIndex);
+    });
+  };
+
   return (
     <main>
       <article className="review">
@@ -17,6 +39,14 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
       </article>
     </main>
   );
