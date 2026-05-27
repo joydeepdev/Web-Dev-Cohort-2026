043c7c5b1d6cc1891631583a148e9511428aaa38
joydeepdev <joydeep6123@gmail.com>
2026-05-27 23:56:09 +0530

    feat(React-Projects/accordian): logic done

--- Projects/React-Projects/accordion/src/App.jsx
+++ Projects/React-Projects/accordion/src/App.jsx
@@ -1,4 +1,23 @@
+import { useState } from 'react';
+import data from './data';
+import Questions from './components/Questions';
+
 const App = () => {
-  return <div>App</div>;
+  const [questions, setQuestions] = useState(data);
+  const [activeId, setActiveId] = useState(null);
+
+  const toggleQuestion = (id) => {
+    const newActiveId = id === activeId ? null : id;
+    setActiveId(newActiveId);
+  };
+  return (
+    <main>
+      <Questions
+        questions={questions}
+        activeId={activeId}
+        toggleQuestion={toggleQuestion}
+      />
+    </main>
+  );
 };
 export default App;
