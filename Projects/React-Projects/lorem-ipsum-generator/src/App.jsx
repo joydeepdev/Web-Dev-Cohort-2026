bfe069b8110d6994c57ed24ddaedf7ae4a8b5210
joydeepdev <joydeep6123@gmail.com>
2026-06-16 22:47:50 +0530

    feat(React-Projects/lorem-ipsum-generator):main functionality done

--- Projects/React-Projects/lorem-ipsum-generator/src/App.jsx
+++ Projects/React-Projects/lorem-ipsum-generator/src/App.jsx
@@ -1,6 +1,39 @@
+import { useState } from 'react';
+import text from './data';
+import { nanoid } from 'nanoid';
+
 const App = () => {
+  const [count, setCount] = useState(1);
+  const [data, setData] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setData(text.slice(0, count));
+  };
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <section>
+      <h1>Tired of boring lorem ipsum</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="paragraph">Paragraphs: </label>
+          <input
+            type="number"
+            name="paragraph"
+            id="paragraph"
+            min="1"
+            max="9"
+            step="1"
+            value={count}
+            onChange={(e) => setCount(Number(e.target.value))}
+          />
+        </div>
+        <button type="submit">Generate</button>
+      </form>
+      <div>
+        {data.map((para) => {
+          return <p key={nanoid()}>{para}</p>;
+        })}
+      </div>
+    </section>
+  );
+};
+export default App;
